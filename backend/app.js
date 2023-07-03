8ca3105ef64da9ac1fba00b4a5b144f58bec286e
Waguti Warren <warrenw639@gmail.com>
2023-07-03 21:04:23 +0300

    setup

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3001, () => console.log('listening on port 3001'));  
