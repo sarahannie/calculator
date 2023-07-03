381d6832a7b469e6fd4ae4f5a2729b92341460df
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-07-04 00:08:42 +0300

    refactor calc logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,54 @@
-import  { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 const App = () => {
+  const [valueOne, setValueOne] = useState(0);
+  const [valueTwo, setValueTwo] = useState(0);
   const [result, setResult] = useState(0);
 
-  const handleButtonClick = (value) => {
-    setResult(result + value);
+  const handleButtonClick = () => {
+    const sum = parseInt(valueOne) + parseInt(valueTwo);
+    setResult(sum);
   };
 
   const handleCalculate = () => {
-    // Logic to calculate the result
+    const sum = parseInt(valueOne) + parseInt(valueTwo);
+    setResult(sum);
   };
- const handleSubmit = () = () => {
 
- }
   return (
-    <div className='container'>
-     <div className="center">
-     <div className='display'>
-        <p>{result}</p>
+    <div className="container">
+      <div className="center">
+        <div className="display">
+          <p>{result}</p>
+        </div>
+        <div>
+          <div>
+            <label>Enter the first value</label>
+            <input
+              type="number"
+              value={valueOne}
+              onChange={(e) => setValueOne(e.target.value)}
+            />
+          </div>
+          <button name="+" onClick={handleButtonClick}>
+            +
+          </button>
+          <div>
+            <label>Enter the second value</label>
+            <input
+              type="number"
+              value={valueTwo}
+              onChange={(e) => setValueTwo(e.target.value)}
+            />
+          </div>
+        </div>
+        <div>
+          <button name="=" onClick={handleCalculate}>
+            =
+          </button>
+        </div>
       </div>
-      <div>
-        <input name="1" onClick={() => handleButtonClick(1)}>1</input>
-        <button name="+" onClick={() => handleButtonClick('+')}>+</button>
-        <button name="2" onClick={() => handleButtonClick(2)}>2</button>
-      </div>
-      <div>
-        <button name="=" onClick={handleCalculate}>=</button>
-      </div>
-     </div>
     </div>
   );
 };
