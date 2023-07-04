db0ed4280313f37116a1b17ec5201d17d2b987be
Johnson11 <kalijoe11@gmail.com>
2023-07-05 00:00:59 +0300

    crated backend image

--- backend/modals/calculations.js
+++ backend/modals/calculations.js
@@ -7,3 +7,10 @@ const History = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("history", History);
+
+// docker run --network host curlimages/curl `
+// >>   --request POST `
+// >>   --url http://localhost:3000/api/calculations `
+// >>   --header 'content-type: application/json' `
+// >>   --data '{"value_one": "string", "value_two": "string", "operand": "string"}'
+// >> 
